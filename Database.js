89547c50baa7fe62042b94e579acf9f542fd09a4
LycheeFish <66574085+LycheeFish@users.noreply.github.com>
2023-11-24 17:15:40 -0500

    Login handler (#117)
    * security database
    
    * security and token verification
    
    * login handler
    
    * handler input modification
    
    * handler edit

--- Database.js
+++ Database.js
@@ -8,11 +8,11 @@ const config = {
     host: 'localhost'
   },
   db: {
-    host:"mysql-b1360a5-sam2001fabian-79c2.a.aivencloud.com",
+    host:"riotgames-riotgames.a.aivencloud.com",
     user:"avnadmin",
-    password:"AVNS_q-_1nPk2Wfg88rY72Nr",
-    port:"10623",
-    database:"defaultdb"
+    password:"AVNS_WB4YIVumgViE7TdZqDV",
+    port:"22473",
+    database:"riotgames"
   }
 };
 
