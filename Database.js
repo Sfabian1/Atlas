a2f5eedff7dc6f8633e67833bba68f7c8ae3f408
LycheeFish <66574085+LycheeFish@users.noreply.github.com>
2023-12-10 18:31:44 -0500

    html cache (#153)

--- Database.js
+++ Database.js
@@ -8,12 +8,12 @@ const config = {
     port: 8000,
     host: 'localhost'
   },
-  db: {
-    host:"mysql-3e3by9365-scarletmail-6b29.a.aivencloud.com",
+  db:{
+    host:"riotgames-riotgames.a.aivencloud.com",
     user:"avnadmin",
-    password:"AVNS_JlLpmJ4TbLc1BfkZ2io",
-    port:"28768",
-    database:"defaultdb"
+    password:"AVNS_WB4YIVumgViE7TdZqDV",
+    port:"22473",
+    database:"riotgames"
 }
 };
 
